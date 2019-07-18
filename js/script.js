a743cdb07c5e25c4733ba0103fad7caa21e1b7f4
petern160 <peternguyen160@gmail.com>
2019-07-17 22:40:50 -0700

    adding in items nad jquery for magnific popup

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  //   $("#header, .info").ripples({
+  //     dropRadius: 10,
+  //     perturbance: 0.01
+  //   });
+
+  $(".parent-container").magnificPopup({
+    delegate: "a",
+    type: "image"
+  });
+});
